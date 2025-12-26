6544e2acfde418652e6ca97e41ea507aa0dc9c9a
Phenyo Motsepe <phenyomotsepe100@gmail.com>
2025-12-26 07:01:41 +0200

    Brief description of changes

---
+++ Task-Manager.py
@@ -0,0 +1,31 @@
+tasks = []
+
+def add_task(title):
+    task = {
+        "title": title,
+        "completed": False
+    }
+    tasks.append(task)
+
+def show_tasks():
+    for index, task in enumerate(tasks):
+        status = "✓" if task["completed"] else "✗"
+        print(f"{index + 1}. {task['title']} [{status}]")
+
+def complete_task(task_number):
+    tasks[task_number - 1]["completed"] = True
+
+while True:
+    print("\n1. Add Task\n2. Show Tasks\n3. Complete Task\n4. Exit")
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        title = input("Task title: ")
+        add_task(title)
+    elif choice == "2":
+        show_tasks()
+    elif choice == "3":
+        number = int(input("Task number: "))
+        complete_task(number)
+    elif choice == "4":
+        break
